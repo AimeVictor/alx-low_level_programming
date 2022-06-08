701afc0e509a0f418b76c588b6b6860899a13438
AimeVictor <aimevictor68@gmail.Com>
2022-06-08 08:54:56 +0200

    operators

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,59 +1,24 @@
 #include "main.h"
-
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-
-int main(void)
-
+*print_sign - to check the sign of a number
+*@n: is character to check
+*Return: 1 n>0 return 0 n=0 returns -1 n<0
+*/
+int print_sign(int n)
 {
-
-    int r;
-
-
-    r = print_sign(98);
-
-    _putchar(',');
-
-    _putchar(' ');
-
-    _putchar(r + '0');
-
-    _putchar('\n');
-
-    r = print_sign(0);
-
-    _putchar(',');
-
-    _putchar(' ');
-
-    _putchar(r + '0');
-
-    _putchar('\n');
-
-    r = print_sign(0xff);
-
-    _putchar(',');
-
-    _putchar(' ');
-
-    _putchar(r + '0');
-
-    _putchar('\n');
-
-    r = print_sign(-1);
-
-    _putchar(',');
-
-    _putchar(' ');
-
-    _putchar(r + '0');
-
-    _putchar('\n');
-
-    return (0);
-
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
