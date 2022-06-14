ff334a0cff047a3de384684de01bcc84aeb91045
AimeVictor <aimevictor68@gmail.Com>
2022-06-14 14:09:44 +0200

    a program to update 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*reset_to_98 - pointer to an int as parameter and updates the value
+*@n: checks an integer
+*Return: Always 0 (sucess)
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
